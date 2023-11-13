16615fad51e40c396ab9e2837057ec6d5a486f0a
trontzal <trontzal@gmail.com>
2023-11-13 20:58:43 +0100

    fracciones empezado

---
+++ EjercicioPoo1/src/main/java/com/puntoCom/ClaseFraccion/Fraccion.java
@@ -0,0 +1,23 @@
+package com.puntoCom.ClaseFraccion;
+
+public class Fraccion {
+	private int num;
+	private int den;
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public int getDen() {
+		return den;
+	}
+
+	public void setDen(int den) {
+		this.den = den;
+	}
+
+}
