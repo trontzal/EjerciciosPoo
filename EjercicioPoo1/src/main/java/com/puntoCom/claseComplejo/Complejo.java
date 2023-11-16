b0ad1c54414cd2b9fa65dadc3d1be1934b6e1951
trontzal <trontzal@gmail.com>
2023-11-16 16:07:45 +0100

    Operaciones con numeros complejos terminada

--- EjercicioPoo1/src/main/java/com/puntoCom/claseComplejo/Complejo.java
+++ EjercicioPoo1/src/main/java/com/puntoCom/claseComplejo/Complejo.java
@@ -1,5 +1,7 @@
 package com.puntoCom.claseComplejo;
 
+import org.apache.tomcat.jakartaee.commons.lang3.builder.EqualsBuilder;
+
 public class Complejo {
 	private double parteReal;
 	private double parteImaginaria;
@@ -29,14 +31,16 @@ public class Complejo {
 
 		return new Complejo(nuevaParteReal, nuevaParteImaginaria);
 	}
-	
+
 	public Complejo multiplicar(Complejo otroComplejo) {
-	    double multiplicacionReal = this.parteReal * otroComplejo.getParteReal() - this.parteImaginaria * otroComplejo.getParteImaginaria();
-	    double multiplicacionImaginaria = this.parteReal * otroComplejo.getParteImaginaria() + this.parteImaginaria * otroComplejo.getParteReal();
-	    
-	    return new Complejo(multiplicacionReal, multiplicacionImaginaria);
+		double multiplicacionReal = this.parteReal * otroComplejo.getParteReal()
+				- this.parteImaginaria * otroComplejo.getParteImaginaria();
+		double multiplicacionImaginaria = this.parteReal * otroComplejo.getParteImaginaria()
+				+ this.parteImaginaria * otroComplejo.getParteReal();
+
+		return new Complejo(multiplicacionReal, multiplicacionImaginaria);
 	}
-	
+
 	// mi logica principal para hacer la multiplicacion
 //	public Complejo multiplicar(Complejo otroComplejo) {
 //		double multiplicacionReal = this.parteReal * otroComplejo.getParteReal();
@@ -48,6 +52,46 @@ public class Complejo {
 //		
 //		return new Complejo(nuevaParteReal, nuevaParteImaginaria);
 //	}
+
+	public Complejo multiplicar(double n) {
+		double realPorN = this.parteReal * n;
+		double imaginarioPorN = this.parteImaginaria * n;
+
+		return new Complejo(realPorN, imaginarioPorN);
+	}
+
+	public Complejo dividir(Complejo otroComplejo) {
+		double c2 = otroComplejo.getParteReal() * otroComplejo.getParteReal();
+		double d2 = otroComplejo.getParteImaginaria() * otroComplejo.getParteImaginaria();
+
+		double aPorC = this.parteReal * otroComplejo.getParteReal();
+		double bPorD = this.parteImaginaria * otroComplejo.getParteImaginaria();
+		double bPorC = this.parteImaginaria * otroComplejo.getParteReal();
+		double aPorD = this.parteReal * otroComplejo.getParteImaginaria();
+
+		double parteRealDivision = (aPorC + bPorD) / (c2 + d2);
+		double parteImaginariaDivision = (bPorC - aPorD) / (c2 + d2);
+
+		return new Complejo(parteRealDivision, parteImaginariaDivision);
+	}
+
+	@Override
+	public String toString() {
+		return "(" + parteReal + ", " + parteImaginaria + ")";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null || getClass() != obj.getClass()) {
+			return false;
+		}
+		Complejo otroComplejo = (Complejo) obj;
+		return Double.compare(otroComplejo.getParteReal(), parteReal) == 0
+				&& Double.compare(otroComplejo.getParteImaginaria(), parteImaginaria) == 0;
+	}
 
 	public double getParteReal() {
 		return parteReal;
--- EjercicioPoo1/src/main/java/com/puntoCom/claseComplejo/complejoPrueba.java
+++ EjercicioPoo1/src/main/java/com/puntoCom/claseComplejo/complejoPrueba.java
@@ -5,20 +5,28 @@ public class complejoPrueba {
 		// Creo los numeros complejos
 		Complejo complejo1 = new Complejo(2.0, 3.0);
 		Complejo complejo2 = new Complejo(1.0, 4.0);
-		
+
 		// Sumo los numeros
 		Complejo resultadoSuma = complejo1.sumar(complejo2);
-		
+
 		// Resto los numeros
 		Complejo resultadoResta = complejo1.restar(complejo2);
-		
+
 		// Multiplico
 		Complejo resultadoMultiplicacion = complejo1.multiplicar(complejo2);
-		
-		
-		//Muestro resultados
-		System.out.println("Suma = "+ resultadoSuma.getParteReal() + " , " + resultadoSuma.getParteImaginaria());
-		System.out.println("Resta = " + resultadoResta.getParteReal() + " , " + resultadoResta.getParteImaginaria());
-		System.out.println("Suma = " + resultadoMultiplicacion.getParteReal() + " , " + resultadoMultiplicacion.getParteImaginaria());
+
+		// Multiplico por un numero
+		Complejo resultadoMultiplicacionPorN = complejo1.multiplicar(3);
+
+		// Division
+		Complejo resultadoDivision = complejo1.dividir(complejo2);
+
+		// Muestro resultados
+		System.out.println("Suma = " + resultadoSuma);
+		System.out.println("Resta = " + resultadoResta);
+		System.out.println("Multiplicacion = " + resultadoMultiplicacion);
+		System.out.println("Multiplico por 3 = " + resultadoMultiplicacionPorN);
+		System.out.println("Division = " + resultadoDivision);
+
 	}
 }
